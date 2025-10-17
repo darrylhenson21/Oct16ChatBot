e85d8422ffe2b66b9a8fd5d3a4bc6aa395795f5f
darrylhenson21 <dada.henson64@gmail.com>
2025-10-17 21:21:06 +0800

    Update supabaseServer.ts

--- nextjs-app/lib/supabaseServer.ts
+++ nextjs-app/lib/supabaseServer.ts
@@ -12,7 +12,7 @@ export const supabaseAdmin = () =>
 export const supabaseAnon = () =>
   createClient(
     process.env.SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       auth: { persistSession: false }
     }
